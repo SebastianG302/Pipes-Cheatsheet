b9e8db235f302aa873f291c3bc7543f1d503de9e
SebastianG302 <johan.guerrero@pi.edu.co>
2022-09-12 23:55:49 -0500

    started numbers page

--- src/app/sales/pages/numbers/numbers.component.ts
+++ src/app/sales/pages/numbers/numbers.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-numbers',
   templateUrl: './numbers.component.html',
   styleUrls: ['./numbers.component.css']
 })
-export class NumbersComponent implements OnInit {
+export class NumbersComponent {
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  netSales: number = 21243254.1235;
+  percentage: number = 0.48;
 
+  
 }
